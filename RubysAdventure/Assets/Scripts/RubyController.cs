ad304138f2828008ff87104764afcecd718e10c8
ajluvscats <aj_349@icloud.com>
2023-11-06 15:26:00 -0800

    Decorating
    I am decorating.

--- RubysAdventure/Assets/Scripts/RubyController.cs
+++ RubysAdventure/Assets/Scripts/RubyController.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { 
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("horizontal");
-        Debug.Log(horizontal);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
         
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
         transform.position = position;
     }
 }
