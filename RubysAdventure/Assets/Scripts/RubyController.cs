a8cce93e739ccdf3481e061b43dffaf1b083d623
ajluvscats <aj_349@icloud.com>
2023-11-28 15:27:33 -0800

    i finished collectable items
    I finished the collectable items and its on damagable items.

--- RubysAdventure/Assets/Scripts/RubyController.cs
+++ RubysAdventure/Assets/Scripts/RubyController.cs
@@ -4,11 +4,14 @@ using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
     public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
     int currentHealth;
+   
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -18,6 +21,7 @@ public class RubyController : MonoBehaviour
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        
     }
 
     // Update is called once per frame
@@ -36,7 +40,7 @@ public class RubyController : MonoBehaviour
 
         rigidbody2d.MovePosition(position);
     }
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth = amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
