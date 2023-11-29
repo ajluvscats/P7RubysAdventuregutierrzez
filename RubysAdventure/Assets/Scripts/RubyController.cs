4f1d8eee4d95f25131446e22abf22d48623f50c3
ajluvscats <aj_349@icloud.com>
2023-11-28 20:06:31 -0800

    i finished damageable objects
    i finished the damageable objects part and is on to world animations.

--- RubysAdventure/Assets/Scripts/RubyController.cs
+++ RubysAdventure/Assets/Scripts/RubyController.cs
@@ -8,9 +8,14 @@ using UnityEngine.UIElements;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
+
     public int maxHealth = 5;
+    public float timeInvincible = 2;
     public int health { get { return currentHealth; } }
     int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
    
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -29,8 +34,15 @@ public class RubyController : MonoBehaviour
     {
          horizontal = Input.GetAxis("Horizontal");
          vertical = Input.GetAxis("Vertical");
-        
-        
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if(invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
     }
     void FixedUpdate()
     {
@@ -42,6 +54,15 @@ public class RubyController : MonoBehaviour
     }
     public void ChangeHealth(int amount)
     {
+        if(amount < 0)
+        {
+            if(isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth = amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
