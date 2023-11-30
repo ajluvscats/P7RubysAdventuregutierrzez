742c734533d6fbe6e3a80487de6d2b16049cccdb
ajluvscats <aj_349@icloud.com>
2023-11-29 20:28:19 -0800

    particles
    I finished camera chimanenfnadnnvj

--- RubysAdventure/Assets/Scripts/EnemyController.cs
+++ RubysAdventure/Assets/Scripts/EnemyController.cs
@@ -10,19 +10,29 @@ public class EnemyController : MonoBehaviour
    
     Rigidbody2D rigidbody2d;
 
+    bool broken;
+
     float timer;
     int direction = 1;
+
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!broken)
+        {
+            return;
+        }
         timer -= Time.deltaTime;
         if (timer < 0)
         {
@@ -33,13 +43,21 @@ public class EnemyController : MonoBehaviour
 
      void FixedUpdate()
     {
+        if(!broken)
+        {
+            return;
+        }
         Vector2 position = rigidbody2d.position;
         if (verticle)
         {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
             position.y = position.y + Time.deltaTime * speed * direction;
         }
         else
         {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
             position.x = position.x = +Time.deltaTime * speed * direction;
         }
 
@@ -53,5 +71,10 @@ public class EnemyController : MonoBehaviour
         {
             player.ChangeHealth(-1);
         }
+    }
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2d.simulated = false;
     }
 }
--- RubysAdventure/Assets/Scripts/RubyController.cs
+++ RubysAdventure/Assets/Scripts/RubyController.cs
@@ -10,13 +10,19 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    public float timeInvincible = 2;
+
+    public GameObject projectilePrefab;
+
+    public float timeInvincible = 2.0f;
     public int health { get { return currentHealth; } }
     int currentHealth;
 
     bool isInvincible;
     float invincibleTimer;
    
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -26,7 +32,7 @@ public class RubyController : MonoBehaviour
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -34,6 +40,16 @@ public class RubyController : MonoBehaviour
     {
          horizontal = Input.GetAxis("Horizontal");
          vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2 (horizontal, vertical);
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if (isInvincible)
         {
@@ -43,6 +59,11 @@ public class RubyController : MonoBehaviour
                 isInvincible = false;
             }
         }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+
     }
     void FixedUpdate()
     {
@@ -56,6 +77,8 @@ public class RubyController : MonoBehaviour
     {
         if(amount < 0)
         {
+            animator.SetTrigger("Hit");
+
             if(isInvincible)
             {
                 return;
@@ -65,5 +88,14 @@ public class RubyController : MonoBehaviour
         }
         currentHealth = Mathf.Clamp(currentHealth = amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position = Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+        animator.SetTrigger("Launch");
     }
 }
